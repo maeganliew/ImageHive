b5a575f88c9a731d3f9bd552944eda11a28f6341
Liew Jia Wei <mjw.liew@gmail.com>
2025-01-31 22:56:54 +0800

    added url

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more");
 
+let keyword = "";
+let page = 1;
+
+async function searchImage() {
+    keyword = searchBox.value;
+    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}';
+}
