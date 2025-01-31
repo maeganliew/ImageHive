9e21889e71c95e4c6fdd4112dd5797a4238c0575
Liew Jia Wei <mjw.liew@gmail.com>
2025-01-31 23:00:10 +0800

    access key

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const accessKey = "brr4e9i7BkpHvv9ytEGRq5G1CmkIoWs5qhKzZH5iUUY";
+
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
@@ -8,5 +10,5 @@ let page = 1;
 
 async function searchImage() {
     keyword = searchBox.value;
-    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}';
+    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
 }
