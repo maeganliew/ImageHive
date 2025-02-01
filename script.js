0a89502382183c270a9b2176ff767958b042de2d
Liew Jia Wei <mjw.liew@gmail.com>
2025-02-01 17:18:50 +0800

    basic functions done

--- script.js
+++ script.js
@@ -16,9 +16,12 @@ async function searchImage() {
 
     const response = await fetch(url); /* 'response' stores result of request, a Response object*/
     const data = await response.json(); /* await is to wait for code to complete before moving on to next line*/
-
     const results = data.results; /*data.results is an array → It contains multiple image objects from the API*/
 
+    if (page === 1) { /*when starting a new search*/
+        searchResult.innerHTML = '';
+    }
+    
     results.map((result) => {  /*code written here will be executed for each element of the array*/
         const image = document.createElement("img");
         image.src = result.urls.small; /* see from inspect*/
@@ -44,3 +47,17 @@ showMoreBtn.addEventListener("click", (e) => {
     page++;
     searchImage();
 })
+
+/* remarks: 
+
+if search 'birds' then 'trees', without the section in line 20, 
+
+then website will diplay pics of birds + trees,
+
+although when form submitted the page variable is assigned to 1,
+
+it still shows multiple results because the new set of results is appended to the already existing search results in search-result,
+
+page is just to reference from api how many pages per pics etc,
+
+content displayed on website depends on search-result*/
